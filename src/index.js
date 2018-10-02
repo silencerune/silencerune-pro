0d79df0d6e593ee8042804525c3498a7ff2589e3
silencerune <silencerune@gmail.com>
2018-10-02 23:00:46 +0800

    SET

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Hw from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Hw />, document.getElementById('root'));
-
-registerServiceWorker();
